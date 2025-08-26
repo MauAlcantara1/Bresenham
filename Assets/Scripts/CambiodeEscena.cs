b732c9ac9fe1e208b40bb41397704bec0e041549
Dakh-o <danielluciano509@aragon.unam.mx>
2025-08-25 19:40:55 -0600

    Script de Boton
    Se agrego el Script mismo usado en conicas y rectas en las dos escenas del proyecto, ademas de su respectivo elemento vacio donde se encuentra dicho script

---
+++ Assets/Scripts/CambiodeEscena.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiodeEscena : MonoBehaviour
+{
+    public void InicioAMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void MainAInicio()
+    {
+
+        SceneManager.LoadScene("Inicio");
+    }
+    public void MainAinformacion()
+    {
+        SceneManager.LoadScene("informacion");
+    }
+
+    public void informacionAMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
+
